820d0bc1ca512d135d7758b3d37dc707cdc9ca94
Jonathan Ibarrola <ibarrolajonathan@gmail.com>
2021-11-25 19:14:06 -0300

    added some coments

--- puertoPc/src/app.js
+++ puertoPc/src/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 /* Routes */
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+/* var usersRouter = require('./routes/users'); */
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 /* Middlewares de routes */
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/* app.use('/users', usersRouter); */
 
 app.use(express.static('public'));
 
