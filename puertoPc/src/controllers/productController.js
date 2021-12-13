8398acda821ab92adddf405a53335608d67335ac
Silvana Zalazar <silvana.blaq@gmail.com>
2021-12-13 14:07:23 -0300

    changes in product controller

--- puertoPc/src/controllers/productController.js
+++ puertoPc/src/controllers/productController.js
@@ -10,6 +10,11 @@ var toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 var productController = {
     	// Root - Show all products
 	products: (req, res) => {
+
+        let idProduct = +req.params.id;
+        let product = products.find(product => product.id === idProduct);
+
+        
 		res.render('products/products', {
             title:"Productos",        
 			products,
@@ -24,6 +29,15 @@ var productController = {
     },
 
     productDetail: (req, res )=> {
+        let products =require('../data/products.json')
+
+        let controller ={
+            product : (req, res) => {
+
+                let   productDetailId = +req.params.id;
+                
+            }
+        }
         res.render('products/productDetail',{
             title: "Detalle de Producto"
         });
