5dd94a77acc62f04d7035581d850589e8a0ce327
Jonathan Ibarrola <ibarrolajonathan@gmail.com>
2022-02-22 20:28:00 -0300

    i worked in productForm.js i captured inputs of createProducts.ej

--- puertoPc/public/js/productForm.js
+++ puertoPc/public/js/productForm.js
@@ -1,1 +1,19 @@
-alert('hola campeon')
+function qs(element) {
+    return document.querySelector(element)
+}
+
+let $inputName = qs('#name'),
+$nameErrors = qs('#nameErrors'),
+$inputDescription = qs('#description'),
+$descriptionErrors = qs('#descriptionErrors'),
+$category = qs('#category'),
+$categoryErrors = qs('#categoryErrors'),
+$inputStock = qs('#stock'),
+$stockErrors = qs('stockErrors'),
+$inputPrice = qs('#price'),
+$priceErrors = qs('#priceErrors'),
+$inputDiscount = qs('#discount'),
+$discountErrors = qs('#discountErrors'),
+$file = qs('#file'),
+$fileErrors = qs('#fileErrors'),
+$imagePreview = qs('#img-preview')
