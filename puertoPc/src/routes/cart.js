edd1e39c047365ba17fbb406d4f9b0a9b4aad1ba
Jonathan Ibarrola <ibarrolajonathan@gmail.com>
2022-03-27 12:06:15 -0300

    i added some styles in product cart

--- puertoPc/src/routes/cart.js
+++ puertoPc/src/routes/cart.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+
 const {show, add, remove, removeItem, empty} = require('../controllers/cartController');
 
 // api/cart
 router
-    .get('/show', show)
+    .get('/show',show)
     .post('/:id',add)
     .delete('/empty',empty)
     .delete('/item/:id',removeItem)
--- puertoPc/src/routes/productRouter.js
+++ puertoPc/src/routes/productRouter.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let router = express.Router();
 let productController = require("../controllers/productController")
+const userCheck = require('../middlewares/userCheck')
 
 /* GET home page. */
 router.get('/detalles/:id', productController.productDetail);
@@ -11,7 +12,7 @@ router.get('/category/:id', productController.productCategory);
 /* GET - categories page general */
 router.get('/category', productController.categories);
 
-router.get('/carrito', productController.productCart);
+router.get('/carrito', userCheck, productController.productCart);
 router.get('/', productController.index);
 /* GET - Search products */
 router.get('/search', productController.productSearch)
